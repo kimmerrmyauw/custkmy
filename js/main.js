cf076544b5eeafad1bfa30a9777c91ceab80a4e5
kimmerrmyauw <kimmyauwcanva@gmail.com>
2026-08-12 20:39:42 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY BIRTHDAY ZALFA!!💕🤍, ILY').split('')
+    const titles = ('HAPPY BIRTHDAY ZALFA!!💕🤍, I LOVE YOU').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
